4cf40332e1181a16beea5a382acdbfc853124265
Trine <trla0001@stud.kea.dk>
2024-02-04 20:51:33 +0100

    product site with json done

--- js/product.js
+++ js/product.js
@@ -4,5 +4,10 @@ fetch("https://kea-alt-del.dk/t7/api/products/1525")
 
 function showProduct(product) {
   console.log(product);
-  document.querySelector(".h3").textContent = product.productdisplayname;
+  document.querySelector(".productdisplayname").textContent = product.productdisplayname;
+  document.querySelector(".price").textContent = product.price;
+  document.querySelector(".model_name").textContent = product.productdisplayname;
+  document.querySelector(".articletype").textContent = product.articletype;
+  document.querySelector(".brandname").textContent = product.brandname;
+  document.querySelector(".id").textContent = product.id;
 }
