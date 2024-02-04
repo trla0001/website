ea983ea275b72fe97cf56a9ae105fa9baad1f96e
Trine <trla0001@stud.kea.dk>
2024-02-04 20:30:37 +0100

    intet virker

---
+++ js/productlist.js
@@ -0,0 +1,26 @@
+fetch("https://kea-alt-del.dk/t7/api/products")
+  .then((res) => res.json())
+  .then((data) => showProducts(data));
+
+function showProducts(products) {
+  //looper og kalder showProducts
+  products.forEach(showProduct);
+}
+function showProduct(product) {
+  //fange template
+  const template = document.querySelector("article").content;
+
+  //lave kopi
+  const copy = template.cloneNode(true);
+
+  //ændre indhold
+  copy.querySelector("img").textContent = product.img;
+  copy.querySelector("productdisplayname").textContent = product.productdisplayname;
+  copy.querySelector("brandname").textContent = product.brandname;
+  copy.querySelector("prev_price").textContent = product.price;
+  copy.querySelector("now_price").textContent = product.price;
+  copy.querySelector("discount").textContent = product.discount;
+
+  //appende
+  document.querySelector("main").appendChild(copy);
+}
